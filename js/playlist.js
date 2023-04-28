7efdcaaa22deaddc342a28322c814ae20a30f2c1
Nassary <89366802+Nassary@users.noreply.github.com>
2023-04-28 22:12:54 +0200

    Add files via upload

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "مجموعه ابتهالات مختارة نادرة جدا",
+    artist: "Syed Naqshbandi",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: " مولاي إني ببابك قد بسطت يدي",
+    artist: "Syed Naqshbandi",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: " النقشبندي ابتهال النفس تشكو ",
+    artist: "Syed Naqshbandi",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "مختارات من اروع ابتهالات الشيخ سيد النقشبندى",
+    artist: "Syed Naqshbandi",
+    img: "img4",
+    audio: "music4"
+  }
+]
